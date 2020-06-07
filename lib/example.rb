6125ddedcd9b0b2787ca2572a379a31e98708955
Sarina Ocal <sarinaocal@gmail.com>
2020-06-07 17:04:49 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,26 +1,26 @@
 # Use this space to code along with the readme
-def say_hello_world_ten_times # Method Signature #leave a line break for the method body
+#def say_hello_world_ten_times # Method Signature #leave a line break for the method body
 phrase = "Hello World!" #Method Body
-  puts "Hello World" #now code the body of this method
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-end #method closing #Immediately Close the method
+  #puts "Hello World" #now code the body of this method
+  #puts phrase
+  #puts phrase
+  #puts phrase
+  #puts phrase
+  #puts phrase
+  #puts phrase
+ # puts phrase
+  #puts phrase
+  #puts phrase
+  #puts phrase
+#end #method closing #Immediately Close the method
 
-say_hello_world_ten_times
-say_hello_world_ten_times
-say_hello_world_ten_times
-say_hello_world_ten_times
-say_hello_world_ten_times
-say_hello_world_ten_times
-say_hello_world_ten_times
-say_hello_world_ten_times
-say_hello_world_ten_times
-say_hello_world_ten_times
+#say_hello_world_ten_times
+#say_hello_world_ten_times
+#say_hello_world_ten_times
+#say_hello_world_ten_times
+#say_hello_world_ten_times
+#say_hello_world_ten_times
+#say_hello_world_ten_times
+#say_hello_world_ten_times
+#say_hello_world_ten_times
+#say_hello_world_ten_times
