123630501ab65a24b26c970ff83622b9e0394160
Sarina Ocal <sarinaocal@gmail.com>
2020-06-07 03:20:15 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,24 @@
 # Use this space to code along with the readme
+def say_hello_world_ten_times # Method Signature #leave a line break for the method body
+phrase = "Hello World!" #Method Body
+  puts "Hello World" #now code the body of this method
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end #method closing #Immediately Close the method
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+say_hello_world_ten_times
+say_hello_world_ten_times
+say_hello_world_ten_times
+say_hello_world_ten_times
+say_hello_world_ten_times
+say_hello_world_ten_times
+say_hello_world_ten_times
+say_hello_world_ten_times
+say_hello_world_ten_times
+say_hello_world_ten_times
---
+++ ruby greeting.rb
@@ -0,0 +1,9 @@
+def greeting
+  puts "Hello World" #now code the body of the method.
+end
+
+greeting # Executing the method by name 
+#=> "Hello World"
+
+greeting # Executing the method again
+#=> "Hello World"
