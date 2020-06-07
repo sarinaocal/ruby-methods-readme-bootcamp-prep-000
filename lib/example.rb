b0681e1295a360c5d15b230b67d037d77e307a09
Sarina Ocal <sarinaocal@gmail.com>
2020-06-07 17:02:45 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,16 +1,16 @@
 # Use this space to code along with the readme
-def say_hello_world_ten_times # Method Signature #leave a line break for the method body
+#def say_hello_world_ten_times # Method Signature #leave a line break for the method body
 phrase = "Hello World!" #Method Body
-  puts "Hello World" #now code the body of this method
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
+  #puts "Hello World" #now code the body of this method
+  #puts phrase
+  #puts phrase
+  #puts phrase
+  #puts phrase
+  #puts phrase
+  #puts phrase
+ # puts phrase
+  #puts phrase
+  #puts phrase
   puts phrase
 end #method closing #Immediately Close the method
 
